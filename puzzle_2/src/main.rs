afc50fa227b53f24e4e2866bd88b2640f4babad1
sukovanej <Milansuk@email.cz>
2017-12-05 16:28:14 +0100

    remove submodules

---
+++ puzzle_2/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("err input");
+
+    let mut sum = 0u32;
+    let mut last = 0;
+    let mut d_chars = input.chars().filter(|x| x != '\n');
+    d_chars = 
+
+    for ch in d_chars {
+        if ch == '\n' { continue; }
+
+        let val = ch.to_string().parse::<u32>().unwrap();
+
+        if last == val {
+            sum += last;
+        }
+
+        last = val;
+    }
+
+    let bytes = input.chars().collect::<Vec<char>>();
+    let mut last_index = bytes.len() - 1;
+
+    while !bytes[last_index].is_digit(10) {
+        last_index -= 1;
+    }
+
+    if bytes[0] == bytes[last_index] {
+        sum += bytes[0].to_string().parse::<u32>().unwrap();
+    }
+
+    println!("{}", sum);
+}
